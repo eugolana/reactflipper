a3c17a1274ae83518d59878683d851e957934e6c
Sam Sharp <eugolana2@gmail.com>
2018-01-25 17:40:27 +0000

    moved <Filters> into a 'functional Component'

--- src/App.js
+++ src/App.js
@@ -28,7 +28,6 @@ class App extends Component {
     }
   }
 
-
   render() {
     return (
         <Board 
@@ -73,6 +72,7 @@ class Board extends Component {
     window.onresize = this.handleResize();
   }
   handleResize() {
+    // Called on window.onresize event
     let _this = this;
     return function() {
       let width = window.innerWidth - (2 * margin);
@@ -177,37 +177,7 @@ class Board extends Component {
     return (
       <div className="board">
         <svg id="boardSVG" height={this.state.height + 20} width={this.state.width + 20}>
-
-        <filter id="dropshadow" height="130%">
-          <feGaussianBlur in="SourceAlpha" stdDeviation="1"/> 
-          <feOffset dx="0.5" dy="0.5" result="offsetblur"/> 
-          <feComponentTransfer>
-            <feFuncA type="linear" slope="2"/>
-          </feComponentTransfer>
-          <feMerge> 
-            <feMergeNode/> 
-            <feMergeNode in="SourceGraphic"/>
-          </feMerge>
-        </filter>
-
-        <filter id="distort">
-          <feTurbulence result="TURBULENCE" baseFrequency="0.5" numOctaves="7" seed="77"  />
-          <feDisplacementMap in="SourceGraphic" in2="TURBULENCE" result="distorted" scale="2" />
-          <feTurbulence result="noise" baseFrequency=".002" numOctaves="2" seed="99" />
-          <feColorMatrix in="noise" result="noisebw" type="saturate" values="0.3" />
-          <feComposite in="noisebw" in2="distorted" operator="in" result="shapedNoise"/>
-          <feBlend in="distorted" in2="shapedNoise" result="noised" mode="multiply"/>
-          <feGaussianBlur in="noised" result="blurred" stdDeviation="0.5"/> 
-
-        </filter>
-
-        <filter id="paperEffect">
-          <feMerge> 
-            <feMergeNode in="SourceGraphic"/>
-          </feMerge>
-
-        </filter>
-
+        <Filters/>
           <rect 
             x={margin} 
             y={margin} 
@@ -433,6 +403,44 @@ function RandomDoubleShapeComp(shapes) {
       </g>
       )
   }
+}
+
+
+
+function Filters(props) {
+  return (
+    <g>
+      <filter id="dropshadow" height="130%">
+        <feGaussianBlur in="SourceAlpha" stdDeviation="1"/> 
+        <feOffset dx="0.5" dy="0.5" result="offsetblur"/> 
+        <feComponentTransfer>
+          <feFuncA type="linear" slope="2"/>
+        </feComponentTransfer>
+        <feMerge> 
+          <feMergeNode/> 
+          <feMergeNode in="SourceGraphic"/>
+        </feMerge>
+      </filter>
+
+      <filter id="distort">
+        <feTurbulence result="TURBULENCE" baseFrequency="0.5" numOctaves="7" seed="77"  />
+        <feDisplacementMap in="SourceGraphic" in2="TURBULENCE" result="distorted" scale="2" />
+        <feTurbulence result="noise" baseFrequency=".002" numOctaves="2" seed="99" />
+        <feColorMatrix in="noise" result="noisebw" type="saturate" values="0.3" />
+        <feComposite in="noisebw" in2="distorted" operator="in" result="shapedNoise"/>
+        <feBlend in="distorted" in2="shapedNoise" result="noised" mode="multiply"/>
+        <feGaussianBlur in="noised" result="blurred" stdDeviation="0.5"/> 
+
+      </filter>
+
+        <filter id="paperEffect">
+        <feMerge> 
+          <feMergeNode in="SourceGraphic"/>
+        </feMerge>  
+
+      </filter>
+      </g>
+        )
 }
 
 function randomColor(colorchoices, bw=false) {
